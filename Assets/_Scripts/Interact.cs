c07128365af52ee83bc918cc4410a2fcd4f0b79f
EthanChampy <35799593+EthanChampy@users.noreply.github.com>
2018-04-18 12:34:27 +0100

    interact

---
+++ Assets/_Scripts/Interact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+    private GameObject RaycastedObj;
+
+    [SerializeField] private int raycastLength = 10;
+    [SerializeField] private LayerMask LayerMaskInteract;
+
+   void Update ()
+    {
+        RaycastHit hit;
+        Vector3 Forward = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, Forward, out hit, raycastLength, LayerMaskInteract.value))
+        {
+            if (hit.collider.CompareTag("Object"))
+            {
+                RaycastedObj = hit.collider.gameObject;
+
+                if (Input.GetKeyDown("e"))
+                {
+                    RaycastedObj.SetActive(false);
+                }
+            }
+
+        }
+	}
+}
